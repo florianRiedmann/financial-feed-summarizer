1226d4845a6bdaa384ef9fbc10e061088a9e37f6
theRealSpoonman <themrspoonman@gmail.com>
2019-12-05 22:25:02 +0100

    venv, first lines of code

--- ffs.py
+++ ffs.py
@@ -5,4 +5,19 @@
 # https://feeds.a.dj.com/rss/RSSMarketsMain.xml             Wall Stree Journal
 # https://www.cnbc.com/id/10000664/device/rss/rss.html      CNBC Finance
 
+# Crawl serveral financial feeds
 # Python Package feedparser
+
+# Determine similarity
+# Jaccard coefficient, Lexical similarity
+# Semantic similarity?
+
+# Create readable 'new' text (Summaries)
+# extractive summarization
+# abstractive summarization
+
+# Text Rank Algorithm
+
+import feedparser
+
+d = feedparser.parse('https://seekingalpha.com/market_currents.xml')
