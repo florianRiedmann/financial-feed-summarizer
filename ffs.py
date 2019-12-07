a2633751c50b43d67ad8394112e27623b3b1b378
theRealSpoonman <themrspoonman@gmail.com>
2019-12-07 15:01:45 +0100

    crawling seekingalpha an getting information from the RSS feed (title, link, text).

--- ffs.py
+++ ffs.py
@@ -19,6 +19,48 @@
 # Text Rank Algorithm
 
 import feedparser
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
 
-d = feedparser.parse('https://seekingalpha.com/market_currents.xml')
-print(d)
+financialFeeds = [  'https://seekingalpha.com/market_currents.xml',
+                    #'https://www.investing.com/rss/news.rss',
+                    #'https://feeds.a.dj.com/rss/RSSMarketsMain.xml',
+                    #'https://www.cnbc.com/id/10000664/device/rss/rss.html'
+                    ]
+
+newsLinks = {'title': [], 'link': []}
+
+# Getting titles and links from the RSS-Feeds and saving into the newsLinks dict
+for feedLink in financialFeeds:
+    d = feedparser.parse(feedLink)
+    for entry in d.entries:
+        newsLinks['title'].append(entry.title)
+        newsLinks['link'].append(entry.link)
+
+# Getting the article text from the link and saving it to the DataFrame (only functional with seekingalpha atm)
+article = []
+
+for link in newsLinks['link']:
+    paragraphText = []
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    if soup.article:
+        paragraphs = soup.article.find_all('p')
+        for paragraph in paragraphs:
+            paragraphText.append(paragraph.get_text())
+        article.append(paragraphText)
+    else:
+        article.append(np.nan)
+
+# Adding paragraph text to the newsLinks dict
+newsLinks['text'] = article
+
+# Converting the dict to a DataFrame and change column order
+financialFeedsDataFrame = pd.DataFrame.from_dict(newsLinks)
+order = ['title', 'link', 'text']
+financialFeedsDataFrame = financialFeedsDataFrame.loc[:,order]
+
+# Export DataFrame to a csv file
+financialFeedsDataFrame.to_csv('financialFeeds.csv', index=False)
