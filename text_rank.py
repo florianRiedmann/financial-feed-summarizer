cbbca1d08f16c1d4747e7839103e41fa9dd9742b
theRealSpoonman <themrspoonman@gmail.com>
2020-01-08 18:00:23 +0100

    text_rank v 1.0

--- text_rank.py
+++ text_rank.py
@@ -1,12 +1,20 @@
-import csv
+import os
 import numpy as np
 import pandas as pd
-from scipy import spatial
 import networkx as nx
 
-sentences = pd.read_csv('test_data/clean_sentences.csv', header=None, squeeze=True, na_filter=False)
-print(sum(sentences.isna()))
+sentences = pd.read_csv('test_data/sentences.csv', header=None, squeeze=True)
+clean_sentences = pd.read_csv('test_data/clean_sentences.csv', header=None, squeeze=True)
+
+# find rows with nan
+mask = clean_sentences.isna()
+nan_rows = clean_sentences[mask].index.tolist()
+
+sentences.drop(nan_rows, inplace=True)
+clean_sentences.drop(nan_rows, inplace=True)
 sentences = sentences.tolist()
+clean_sentences = clean_sentences.tolist()
+
 
 # word_vectors
 # import pre-trained word_vectors from glove
@@ -19,6 +27,7 @@ def get_word_vectors(filepath):
             coefficients = np.array(values[1:], dtype='float32')
             word_vectors[word] = coefficients
     return word_vectors
+
 word_vectors = get_word_vectors(filepath='glove/glove.6B.100d.txt')
 
 # Averaging the word_vectors for the sentence_vector
@@ -27,9 +36,7 @@ word_vectors = get_word_vectors(filepath='glove/glove.6B.100d.txt')
 # https://nlp.stanford.edu/~socherr/EMNLP2013_RNTN.pdf
 
 sentence_vectors = []
-for i in sentences:
-    if type(i) == float:
-        print(i)
+for i in clean_sentences:
     if len(i) != 0:
         sum = np.zeros((100,))
         words = i.split()
@@ -44,24 +51,33 @@ for i in sentences:
         avg = np.zeros((100,))
     sentence_vectors.append(avg)
 
-# Similarity Matrix n x n
+# Similarity Matrix
 n = len(sentences)
 S = np.zeros([n, n])
+
+def cosine_similarity(v1, v2):
+    dot = np.dot(v1, v2)
+    normv1 = np.linalg.norm(v1)
+    normv2 = np.linalg.norm(v2)
+    cos = dot / (normv1 * normv2)
+    return cos
 
 # http://blog.christianperone.com/2013/09/machine-learning-cosine-similarity-for-vector-space-models-part-iii/
 # Cosine similarity between sentence_vectors (cosine similarity: 1 - cosine distance)
 for i in range(n):
   for j in range(n):
     if i != j:
-      S[i][j] = 1 - spatial.distance.cosine(sentence_vectors[i], sentence_vectors[j])
+      S[i][j] = cosine_similarity(sentence_vectors[i], sentence_vectors[j])
 
-print(S)
-
+# Pagerank Algorithm
 # https://networkx.github.io/documentation/networkx-1.9.1/overview.html
-nx_graph = nx.from_numpy_array(S)
-scores = nx.pagerank(nx_graph)
+G = nx.from_numpy_array(S)
+scores = nx.pagerank(G)
 
-ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
+# Tuples of the score and the sentence
+list = []
+for index, sentence in enumerate(sentences):
+    list.append((scores[index], sentence))
 
-for i in range(10):
-  print(ranked_sentences[i][1])
+df = pd.DataFrame(list, columns =['Score', 'Sentence'])
+print(df.nlargest(10, 'Score'))
