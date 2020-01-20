964933acf1dea5532479c8e7e125cb27b3ef0d9c
Thomas Schweiger <th.schweiger@gmx.net>
2020-01-20 21:10:26 +0100

    cleanup

--- scraper.py
+++ scraper.py
@@ -1,11 +1,11 @@
-import os
-import feedparser
-from bs4 import BeautifulSoup
-import requests
-import re
-import pandas as pd
-import numpy as np
 import json
+import os
+import re
+
+import feedparser
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
 project_dir = os.path.dirname(__file__)
 
